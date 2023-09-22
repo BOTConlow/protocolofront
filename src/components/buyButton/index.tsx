992d453ea8b7ce4926c2c7511e58dab52c0100e4
Victor Rios <uskenmellorios@gmail.com>
2023-09-22 18:25:29 -0300

    up

--- src/components/buyButton/index.tsx
+++ src/components/buyButton/index.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 export default function BuyButton({ title }: ButtonProps) {
   return (
     <>
-      <ButtonContainer href="https://pay.kiwify.com.br/rcNH140">
+      <ButtonContainer href="https://pay.slatpay.com/4QN9IS">
         <button>{title}</button>
       </ButtonContainer>
     </>
